dfc3013fd1e3be4b9807c4117ec56fd01c0c8c27
davispacheco <davipacheco1996@gmail.com>
2022-07-26 14:59:24 -0300

    Adicionar construtor e métodos get e set à classe Organizacao.

--- src/Organizacao.java
+++ src/Organizacao.java
@@ -9,4 +9,33 @@ public class Organizacao {
     private Set<Venda> vendas = new HashSet<>();
     private Set<Vendedor> vendedores = new HashSet<>();
 
+    public Organizacao() {
+        this.clientes = clientes;
+        this.vendas = vendas;
+        this.vendedores = vendedores;
+    }
+
+    public Set<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(Set<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public Set<Venda> getVendas() {
+        return vendas;
+    }
+
+    public void setVendas(Set<Venda> vendas) {
+        this.vendas = vendas;
+    }
+
+    public Set<Vendedor> getVendedores() {
+        return vendedores;
+    }
+
+    public void setVendedores(Set<Vendedor> vendedores) {
+        this.vendedores = vendedores;
+    }
 }
