94af4b31dad133b8c95dab4ceea85558a876e767
eveolegario <evelynolegario93@gmail.com>
2022-07-26 14:38:29 -0300

    criação Classe Venda

--- src/Organizacao.java
+++ src/Organizacao.java
@@ -4,6 +4,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Organizacao {
+
+    private Set<Venda> vendas = new HashSet<>();
     private Set<Cliente> clientes = new HashSet<>();
+    private Set<Vendedor> vendedores = new HashSet<>();
 
 }
