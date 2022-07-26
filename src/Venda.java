03fb777596fe53289fc86cb6c36a010d01afabf6
eveolegario <evelynolegario93@gmail.com>
2022-07-26 16:25:27 -0300

    criação de atributos para Classe Venda

--- src/Venda.java
+++ src/Venda.java
@@ -6,4 +6,33 @@ public class Venda {
     private String data;
     private String vendedorResponsavel;
 
+    public Venda(String numeroRegistro, String data, String vendedorResponsavel) {
+        this.numeroRegistro = numeroRegistro;
+        this.data = data;
+        this.vendedorResponsavel = vendedorResponsavel;
+    }
+
+    public String getNumeroRegistro() {
+        return numeroRegistro;
+    }
+
+    public void setNumeroRegistro(String numeroRegistro) {
+        this.numeroRegistro = numeroRegistro;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getVendedorResponsavel() {
+        return vendedorResponsavel;
+    }
+
+    public void setVendedorResponsavel(String vendedorResponsavel) {
+        this.vendedorResponsavel = vendedorResponsavel;
+    }
 }
