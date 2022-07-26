ae42b609289c97a6be77f7eb3a4d7eebab8305c2
p3rsidh <persidh.pimenta@gmail.com>
2022-07-26 15:42:41 -0300

    Configurando cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,8 +1,31 @@
 package src;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Cliente extends Pessoa{
+
+    private String nome;
+    private String cpf;
+    private String email;
+
+    Scanner scan = new Scanner(System.in);
+    private Set<Cliente> clientes = new HashSet<>();
+    public Cliente(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+
     @Override
     public void cadastrar(Organizacao organizacao){
+        System.out.println("Qual o nome do cliente?");
+        String novoNome = scan.next();
+
+
+
 
 
     }
--- src/Organizacao.java
+++ src/Organizacao.java
@@ -4,38 +4,12 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Organizacao {
+    public static void main(String[] args) {
 
-    private Set<Cliente> clientes = new HashSet<>();
-    private Set<Venda> vendas = new HashSet<>();
-    private Set<Vendedor> vendedores = new HashSet<>();
+//    private Set<Venda> vendas = new HashSet<>();
+//    private Set<Vendedor> vendedores = new HashSet<>();
 
-    public Organizacao() {
-        this.clientes = clientes;
-        this.vendas = vendas;
-        this.vendedores = vendedores;
-    }
 
-    public Set<Cliente> getClientes() {
-        return clientes;
-    }
 
-    public void setClientes(Set<Cliente> clientes) {
-        this.clientes = clientes;
-    }
-
-    public Set<Venda> getVendas() {
-        return vendas;
-    }
-
-    public void setVendas(Set<Venda> vendas) {
-        this.vendas = vendas;
-    }
-
-    public Set<Vendedor> getVendedores() {
-        return vendedores;
-    }
-
-    public void setVendedores(Set<Vendedor> vendedores) {
-        this.vendedores = vendedores;
-    }
+}
 }
