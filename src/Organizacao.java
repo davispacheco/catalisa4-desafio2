87a542b6c9d57024f71cf028b6d3466bad676b5c
davispacheco <davipacheco1996@gmail.com>
2022-07-26 18:59:06 -0300

    Apagar classe Main e adicionar o conteúdo dela à classe Organizacao.

--- src/Organizacao.java
+++ src/Organizacao.java
@@ -1,23 +1,28 @@
 package src;
 
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Organizacao {
     public static void main(String[] args) {
-
-        Cliente clienteInicial = new Cliente(null, null, null);
-
-        clienteInicial.listar();
-        clienteInicial.cadastrar();
-        clienteInicial.cadastrar();
-
-        ClienteIncial.listar();
-
-//    private Set<Venda> vendas = new HashSet<>();
-//    private Set<Vendedor> vendedores = new HashSet<>();
-
-
-
-}
+        boolean repeticaoMenu = true;
+        Scanner input = new Scanner(System.in);
+        while (repeticaoMenu) {
+            System.out.println("O que deseja fazer?");
+            System.out.println("1: Cadastrar cliente");
+            System.out.println("2: Cadastrar vendedor");
+            System.out.println("3: Cadastrar venda");
+            System.out.println("4: Listar clientes cadastrados");
+            System.out.println("5: Listar vendedores cadastrados");
+            System.out.println("6: Listar vendas cadastradas");
+            System.out.println("7: Finalizar");
+            String opcao = input.next();
+            switch (opcao) {
+                case "1":
+                    Cliente cliente = new Cliente(); //coloca o metodo adicionar cliente
+            }
+        }
+        input.close();
+    }
 }
