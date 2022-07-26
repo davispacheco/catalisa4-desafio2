ccd00b09965d253d4acc5ddd0aa1566cd48438fe
p3rsidh <persidh.pimenta@gmail.com>
2022-07-26 16:05:49 -0300

    Cadastrar e Listar Cliente

--- src/Organizacao.java
+++ src/Organizacao.java
@@ -6,6 +6,11 @@ import java.util.Set;
 public class Organizacao {
     public static void main(String[] args) {
 
+        Cliente ClienteIncial = new Cliente(null, null, null);
+
+        ClienteIncial.cadastrar();
+        ClienteIncial.listar();
+
 //    private Set<Venda> vendas = new HashSet<>();
 //    private Set<Vendedor> vendedores = new HashSet<>();
 
