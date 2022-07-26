ed71732891c1247c549eeade85f67e4c7858dd12
davispacheco <davipacheco1996@gmail.com>
2022-07-26 15:02:35 -0300

    Adicionar construtor e métodos get e set à classe Pessoa.

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -6,4 +6,33 @@ public abstract class Pessoa {
     private String cpf;
     private String email;
 
+    public Pessoa() {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
