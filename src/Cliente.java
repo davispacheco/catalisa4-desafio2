ce364f60cf8b76dc48b7679737fe79658358a9bb
p3rsidh <persidh.pimenta@gmail.com>
2022-07-26 15:25:00 -0300

    Utilizando metodos em Cliente e Vendedor

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,5 +1,14 @@
 package src;
 
 public class Cliente extends Pessoa{
+    @Override
+    public void cadastrar(){
+
+
+    }
+    @Override
+    public void listar(){
+
+    }
 
 }
--- src/Vendedor.java
+++ src/Vendedor.java
@@ -3,4 +3,13 @@ package src;
 public class Vendedor extends Pessoa{
 
 
+    @Override
+    public void cadastrar() {
+
+    }
+
+    @Override
+    public void listar() {
+
+    }
 }
